86f0ce6bb012a04792b0dd234daee1cfe641ae8d
Andrew Stewart <awstewar@gmail.com>
2016-01-08 22:51:57 -0600

    Adding comments to brew file

--- brew.sh
+++ brew.sh
@@ -1,10 +1,13 @@
+#ask for root pwd so don't have to keep typing in
 sudo -v
 
+#install brew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 #install cask
 brew tap caskroom/cask
 
+#install common apps
 brew install wget
 brew install the_silver_searcher
 brew install cask
